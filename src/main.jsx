bfc31ae75acfed4b146ee8cc45d2c6cebeb26eb4
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2025-03-13 05:46:56 +0800

    基本套件版本調整+基本week6路由設定

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+//import './index.css'
+//import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+;
+// import router from './router/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
   </StrictMode>,
 )
---
+++ src/router/index.jsx
@@ -0,0 +1,49 @@
+import { createHashRouter } from "react-router-dom";
+// import FonterLayout from "../layouts/FonterLayout";
+// import HomePage from "../pages/HomePage";
+// import ProductsPage from "../pages/ProductsPage";
+// import CartPage from "../pages/CartPage";
+// import ProductDetailPage from "../pages/ProductDetailPage"
+// import NotFoundPage from "../pages/NotFoundPage";
+// import LoginPage from "../pages/admin/LoginPage";
+// import DashboardPage from "../pages/admin/DashboardPage";
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <FonterLayout />,
+    children:[
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      { // 產品列表
+        path: 'products',
+        element: <ProductsPage />,
+      },
+      { // 產品細項，如果是多個參數寫法=> path: 'product/:product_id/:typemode'
+        // 要注意參數順序重要：網址的參數順序必須與路由設定一致。
+        path: 'product/:product_id',
+        element: <ProductDetailPage />,
+      },
+      {
+        path: 'cart',
+        element: <CartPage />,
+      }
+    ]
+  },
+  { // Login頁面
+    path:'/login',
+    element:<LoginPage />
+  },
+  { // Admin - Dashboard頁面
+    path:'/dashboard',
+    element:<DashboardPage />
+  },
+  { // 404頁面
+    path:'*',
+    element:<NotFoundPage />
+  }
+]);
+
+export default router;
